487248f2d115131fcdb030cf8f7513c40be04f26
PranjalMantri <pranjalmantri@gmail.com>
2024-02-09 16:59:07 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subsciber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
