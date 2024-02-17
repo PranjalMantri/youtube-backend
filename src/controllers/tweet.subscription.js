46b358d4bb7fe31ee86c1bbeea73da2136f79849
PranjalMantri <pranjalmantri@gmail.com>
2024-02-17 13:01:59 +0530

    added createTweet functionality

--- src/controllers/tweet.subscription.js
+++ src/controllers/tweet.subscription.js
@@ -1,7 +1,28 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: Create a tweet
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(404, "Content is required");
+  }
+
+  const user = await User.findOne({
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  const tweet = await Tweet.create({
+    owner: user._id,
+    content,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfuly"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
--- src/routes/tweet.router.js
+++ src/routes/tweet.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { createTweet } from "../controllers/tweet.subscription.js";
 
 const router = Router();
 router.use(verifyJWT);
 
+router.route("/").post(createTweet);
 export default router;
