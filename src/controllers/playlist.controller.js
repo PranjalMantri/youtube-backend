796b272493ad48c5bb7316f1fde5d659cd581f6f
PranjalMantri <pranjalmantri@gmail.com>
2024-02-17 17:20:50 +0530

    added getPlaylistById functionality

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import { Playlist } from "../models/playlist.model.js";
+import { isValidObjectId } from "mongoose";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
@@ -40,4 +41,56 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, playlist, "Playlist created successfuly"));
 });
 
-export { createPlaylist };
+const getUserPlaylist = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const playlist = await Playlist.find({
+    owner: userId,
+  });
+
+  if (!playlist) {
+    throw new ApiError(400, "User does not have any platlist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "User playlists fetched successfuly"));
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  //TODO: get playlist by id
+  // get playlist id
+  // validate playlist id
+  // return the playlist with that id
+
+  console.log("Getting user playlists");
+  const { playlistId } = req.params;
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist Id");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(500, "Something went wrong while fetching playlists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, "Successfuly fetched the playlist by Id")
+    );
+});
+
+export { createPlaylist, getUserPlaylist, getPlaylistById };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
@@ -8,5 +12,7 @@ router.use(verifyJWT);
 // secure routes
 
 router.route("/").post(createPlaylist);
+router.route("/:userId").get(getUserPlaylist);
+router.route("/p/:playlistId").get(getPlaylistById);
 
 export default router;
